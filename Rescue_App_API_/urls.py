6b1320302c14aad30ba59ebc3048af886c32bcb4
Chris Horton <Chris.horton@sbcglobal.net>
2019-02-18 10:28:05 -0800

    change import

--- Rescue_App_API_/urls.py
+++ Rescue_App_API_/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include # new
+from django.urls import include # new
 
 
 from rest_framework.authtoken import views as rest_framework_views
