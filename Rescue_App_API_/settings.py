bdcd7f8bbd640250df7edb20bb4601a2f0b92a54
Chris Horton <Chris.horton@sbcglobal.net>
2019-02-17 22:48:19 -0800

    go back

--- Rescue_App_API_/settings.py
+++ Rescue_App_API_/settings.py
@@ -75,12 +75,12 @@ DATABASES = {
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'PetPosts',
     'Users.apps.UsersConfig'
 ]
