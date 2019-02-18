7b0d974f435a8eaa392643899bf8f19cc4713731
Chris Horton <Chris.horton@sbcglobal.net>
2019-02-17 22:50:20 -0800

    ahhh

--- PetPosts/models.py
+++ PetPosts/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from Rescue_App_API_ import settings
+
 # Create your models here.
 
 class PetPost(models.Model):
     title                = models.CharField(max_length=255)
     post_id              = models.CharField(max_length=255, blank=True)
-    user                 = models.ForeignKey(User, on_delete=models.CASCADE)
+    user                 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     lat                  = models.FloatField()
     long                 = models.FloatField()
     image_link           = models.TextField()
