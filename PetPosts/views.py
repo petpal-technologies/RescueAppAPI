bc8d0d1e7293694492cfc4bb1f8518f1adcd2c09
Chris Horton <Chris.horton@sbcglobal.net>
2019-02-19 12:39:12 -0800

    change function  names in petpost view

--- PetPosts/views.py
+++ PetPosts/views.py
@@ -16,21 +16,21 @@ class PostView(APIView):
         serializer = PostSerializer(posts, many=True)
         return Response({"posts": serializer.data})
 
-def create(self, request):
-    post = request.data.get('post')
+    def post(self, request):
+        post = request.data.get('post')
 
-    serializer = PostSerializer(data=post)
-    if serializer.is_valid(raise_exception=True):
-        saved_post = serializer.save()
-        return Response({"success": "Post '{}' created successfully".format(saved_post.title)})
-    else:
-        return Response({"failure": "Post failed"})
+        serializer = PostSerializer(data=post)
+        if serializer.is_valid(raise_exception=True):
+            saved_post = serializer.save()
+            return Response({"success": "Post '{}' created successfully".format(saved_post.title)})
+        else:
+            return Response({"failure": "Post failed"})
 
-def delete(self, request, pk):
-    # Get object with this pk
-    post = get_object_or_404(PetPost.objects.all(), pk=pk)
-    post.delete()
-    return Response({"message": "Post with id `{}` has been deleted.".format(pk)}, status=204)
+    def delete(self, request, pk):
+        # Get object with this pk
+        post = get_object_or_404(PetPost.objects.all(), pk=pk)
+        post.delete()
+        return Response({"message": "Post with id `{}` has been deleted.".format(pk)}, status=204)
 
 
 # class PostViewSet(viewsets.ModelViewSet):
