a8f288c2dd0c1cf75e4739dc6edee12fed7e630c
Chris Horton <Chris.horton@sbcglobal.net>
2019-02-19 12:26:21 -0800

    change url

--- Rescue_App_API_/urls.py
+++ Rescue_App_API_/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     url('auth/signup', views.signup),
 
 
-    url('/api/new_post', petView.PostView.create),
-    url('/api/delete', petView.PostView.delete),
-    url('/api/getPosts', petView.PostView.get)
+    url('api/new_post', petView.PostView.create),
+    url('api/delete', petView.PostView.delete),
+    url('api/getPosts', petView.PostView.get)
 ]
