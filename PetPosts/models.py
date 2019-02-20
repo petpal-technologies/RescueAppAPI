f6948030ab3e5098c263ff8379b23fc59026fcd5
Chris Horton <Chris.horton@sbcglobal.net>
2019-02-20 12:41:38 -0800

    Add images

--- PetPosts/models.py
+++ PetPosts/models.py
@@ -11,7 +11,7 @@ class PetPost(models.Model):
     post_id              = models.CharField(max_length=255, blank=True)
     lat                  = models.FloatField()
     long                 = models.FloatField()
-    image                = models.ImageField()
+    image                = models.ImageField(upload_to="posts/images/", null=True, blank=True)
     description          = models.TextField()
     location_description = models.TextField()
     created              = models.DateTimeField(auto_now=True)
