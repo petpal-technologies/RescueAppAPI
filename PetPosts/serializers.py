58f6cd2aea8137a32af9a857e7ce7800153db62c
Chris Horton <Chris.horton@sbcglobal.net>
2019-02-20 14:30:09 -0800

    Add images

--- PetPosts/serializers.py
+++ PetPosts/serializers.py
@@ -5,15 +5,15 @@ from Rescue_App_API_ import settings
 
 
 class PostSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=255)
-    post_id = serializers.CharField(max_length=255)
-    lat = serializers.FloatField()
-    long = serializers.FloatField()
-    image = serializers.ImageField()
-    description = serializers.CharField()
-    location_description = serializers.CharField()
-    created = serializers.DateTimeField()
-    modified = serializers.DateTimeField()
+    # title = serializers.CharField(max_length=255)
+    # post_id = serializers.CharField(max_length=255)
+    # lat = serializers.FloatField()
+    # long = serializers.FloatField()
+    # image = serializers.ImageField()
+    # description = serializers.CharField()
+    # location_description = serializers.CharField()
+    # created = serializers.DateTimeField()
+    # modified = serializers.DateTimeField()
 
     # def create(self, validated_data):
     #     return PetPost.objects.create(**validated_data)
