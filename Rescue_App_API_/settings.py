2035286861135bd81fcd61043b7f85260a9c6207
Chris Horton <Chris.horton@sbcglobal.net>
2019-02-20 20:49:45 -0800

    media urls

--- Rescue_App_API_/settings.py
+++ Rescue_App_API_/settings.py
@@ -14,9 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
