47fb394979d3bf9bb599eccc722a0b3d36c65e15
Chris Horton <Chris.horton@sbcglobal.net>
2019-03-04 11:04:21 -0800

    Change signup logic

--- Users/views.py
+++ Users/views.py
@@ -8,8 +8,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login, logout
 from django.contrib.auth import authenticate
 
-from rest_framework import status
-
 from . import serializers
 from . import models
 
@@ -35,14 +33,20 @@ def auth_login(request):
 @csrf_exempt
 def signup(request):
     if models.CustomUser.objects.filter(username=request.POST['username']).exists():
-        return HttpResponse(status=403)
+        # Login the user if username exists
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        login(request,user)
+        serializer = serializers.UserSerializer(user)
     else:
         u = models.CustomUser(username=request.POST['username'])
         u.set_password(request.POST['password'])
         u.save()
         login(request, u)
         serializer = serializers.UserSerializer(u)
-        return JsonResponse(serializer.data)
+
+    return JsonResponse(serializer.data)
 
 def auth_logout(request):
     """Clears the session """
