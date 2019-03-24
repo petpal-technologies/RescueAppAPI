0c80c31c8e983f4e7a949e6631b744122c725d41
Chris Horton <Chris.horton@sbcglobal.net>
2019-03-23 22:38:26 -0700

    Add replies

--- Comments/views.py
+++ Comments/views.py
@@ -7,13 +7,14 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from PetPosts.models import PetPost
 from django.views.generic import View
-from Rescue_App_API_ import settings
+from django.contrib.auth import get_user_model
+
 
 
 class LoginView(View):
     def post(self, request, *args, **kwargs):
         post = get_object_or_404(PetPost, id=request.POST.get("post_id"))
-        author = get_object_or_404(settings.AUTH_USER_MODEL, id=request.POST.get("author_id"))
+        author = get_object_or_404(get_user_model(), id=request.POST.get("author_id"))
         Comment.objects.create(post=post, text=request.POST.get("text"), author=author)
 
     def get(self, request):
