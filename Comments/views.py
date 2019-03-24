2d100bc3833436675636b9867eb81c633a7182ce
Chris Horton <Chris.horton@sbcglobal.net>
2019-03-23 21:58:13 -0700

    Add replies

--- Comments/views.py
+++ Comments/views.py
@@ -11,10 +11,7 @@ from django.views.generic import View
 
 class LoginView(View):
     def post(self, request, *args, **kwargs):
-        return Response(kwargs.get("post_id"))
-
-
-        post = get_object_or_404(PetPost, id=kwargs.get("post_id", None))
+        post = get_object_or_404(PetPost, pk=kwargs.get("post_id", None))
         Comment.objects.create(post=post, text=kwargs.get("text"))
 
     def get(self, request):
