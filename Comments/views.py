a98282ce0aa0534726ac569c3698e8640e19262d
Chris Horton <Chris.horton@sbcglobal.net>
2019-03-23 22:35:52 -0700

    Add replies

--- Comments/views.py
+++ Comments/views.py
@@ -7,16 +7,17 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from PetPosts.models import PetPost
 from django.views.generic import View
+from Rescue_App_API_ import settings
 
 
 class LoginView(View):
     def post(self, request, *args, **kwargs):
         post = get_object_or_404(PetPost, id=request.POST.get("post_id"))
-        Comment.objects.create(post=post, text=request.POST.get("text"), author_id=request.POST.get("author_id"))
+        author = get_object_or_404(settings.AUTH_USER_MODEL, id=request.POST.get("author_id"))
+        Comment.objects.create(post=post, text=request.POST.get("text"), author=author)
 
     def get(self, request):
         post = get_object_or_404(PetPost, id=request.GET['post_id'])
-        return Response({"post_id": post.id})
         comments = post.comments.all()
         serialized = CommentSerializer(comments, many=True)
         return Response({"comments": serialized.data})
