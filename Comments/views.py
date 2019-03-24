ba0e4a8f88a53d8d6b70081aea43f3931af11624
Chris Horton <Chris.horton@sbcglobal.net>
2019-03-23 22:14:10 -0700

    Add replies

--- Comments/views.py
+++ Comments/views.py
@@ -12,7 +12,7 @@ from django.views.generic import View
 class LoginView(View):
     def post(self, request, *args, **kwargs):
         post = get_object_or_404(PetPost, id=request.POST.get("post_id"))
-        Comment.objects.create(post=post, text=kwargs.get("text"))
+        Comment.objects.create(post=post, text=request.POST.get("text"))
 
     def get(self, request):
         post = get_object_or_404(PetPost, id=request.GET['post_id'])
