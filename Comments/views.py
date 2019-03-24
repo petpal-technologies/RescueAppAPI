f51b492eb0acbf90db0118b6f41fe1cc82df2af8
Chris Horton <Chris.horton@sbcglobal.net>
2019-03-23 22:42:43 -0700

    Add replies

--- Comments/views.py
+++ Comments/views.py
@@ -12,9 +12,9 @@ from django.contrib.auth import get_user_model
 
 
 class LoginView(View):
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         post = get_object_or_404(PetPost, id=request.POST.get("post_id"))
-        author = get_object_or_404(get_user_model(), id=request.POST.get("author_id"))
+        author = get_object_or_404(get_user_model(), hash_id=request.POST.get("author_id"))
         Comment.objects.create(post=post, text=request.POST.get("text"), author=author)
 
     def get(self, request):
