cdb5ac44354f04e8d4b678120fbe03a7483054ee
Chris Horton <Chris.horton@sbcglobal.net>
2019-03-23 22:47:28 -0700

    Add replies

--- Comments/views.py
+++ Comments/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import get_object_or_404
 from PetPosts.models import PetPost
 from django.views.generic import View
 from django.contrib.auth import get_user_model
-
+from django.http import JsonResponse
 
 
 class CommentView(View):
@@ -16,7 +16,7 @@ class CommentView(View):
         post = get_object_or_404(PetPost, id=request.POST.get("post_id"))
         author = get_object_or_404(get_user_model(), hash_id=request.POST.get("author_id"))
         Comment.objects.create(post=post, text=request.POST.get("text"), author=author)
-        return Response({"success": True})
+        return JsonResponse({"success": True}, status=200)
 
     def get(self, request):
         post = get_object_or_404(PetPost, id=request.GET['post_id'])
